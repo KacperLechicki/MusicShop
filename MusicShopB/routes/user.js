b7f8ad99f6c577140e08202de2c61d6a87dc9d44
kacperlechicki <klechicki@poczta.fm>
2023-02-24 15:52:56 +0100

    login req v1 working

--- MusicShopB/routes/user.js
+++ MusicShopB/routes/user.js
@@ -33,13 +33,17 @@ router.post('/login', (req, res) => {
 	query = `select email, password, role, status from usert where email = '${user.email}'`;
 	connection.query(query, (err, results) => {
 		if (!err) {
-			if (results.rows.length <= 0 || results[0].password != user.password) {
+			if (
+				results.rows.length <= 0 ||
+				results.rows[0].password != user.password
+			) {
 				return res
-					.statusCode(401)
+					.status(401)
 					.json({ message: `Incorect username or password` });
-			} else if (results[0].status === 'false') {
+			} else if (results.rows[0].status === 'false') {
 				return res.status(401).json({ message: 'Wait for admin approval...' });
-			} else if (results[0].password == user.password) {
+			} else if (results.rows[0].password == user.password) {
+				return res.status(200).json({ message: 'Logged in successfully' });
 			} else {
 				return res
 					.status(400)
