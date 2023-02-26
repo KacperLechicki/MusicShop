72a41e41793f1569d4aa5f4cf2c7e9b61e835ac2
kacperlechicki <klechicki@poczta.fm>
2023-02-26 21:08:30 +0100

    category API works

--- MusicShopB/index.js
+++ MusicShopB/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const userRoute = require('./routes/user');
+const categoryRoute = require('./routes/category');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/user', userRoute);
+app.use('/category', categoryRoute);
 
 module.exports = app;
--- MusicShopB/routes/category.js
+++ MusicShopB/routes/category.js
@@ -3,3 +3,45 @@ const connection = require('../connection');
 const router = express.Router();
 let auth = require('../services/auth');
 let checkRole = require('../services/checkRole');
+
+router.post('/add', auth.auth, checkRole.checkRole, (req, res, next) => {
+	let category = req.body;
+	query = `insert into category (name) values ('${category.name}')`;
+	connection.query(query, (err, results) => {
+		if (!err) {
+			return res.status(200).json({ message: 'Category added successfully' });
+		} else {
+			return res.status(500).json(err);
+		}
+	});
+});
+
+router.get('/get', auth.auth, (req, res, next) => {
+	query = `select * from category order by name`;
+	connection.query(query, (err, results) => {
+		if (!err) {
+			return res.status(200).json(results.rows);
+		} else {
+			return res.status(500).json(err);
+		}
+	});
+});
+
+router.patch('/update', auth.auth, checkRole.checkRole, (req, res, next) => {
+	let category = req.body;
+	query = `update category set name = '${category.name}' where id = ${category.id}`;
+	connection.query(query, (err, results) => {
+		if (!err) {
+			if (results.affectedRows == 0) {
+				return res
+					.statusCode(404)
+					.json({ message: 'Category id is not exist!' });
+			}
+			return res.status(200).json({ message: 'Category updated successfully' });
+		} else {
+			return res.status(500).json(err);
+		}
+	});
+});
+
+module.exports = router;
