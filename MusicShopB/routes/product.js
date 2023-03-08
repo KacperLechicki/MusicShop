78725eb2d227691789c36537e6e2692f791aa843
kacperlechicki <klechicki@poczta.fm>
2023-03-08 21:03:29 +0100

    get products works

--- MusicShopB/routes/product.js
+++ MusicShopB/routes/product.js
@@ -6,10 +6,21 @@ let checkRole = require('../services/checkRole');
 
 router.post('/add', auth.auth, checkRole.checkRole, (req, res) => {
 	let product = req.body;
-	query = `insert into product (name, categoryId, description, price, status) values ('${product.name}', ${product.categoryId}, '${product.description}', ${product.price}, '${product.status}')`;
+	let query = `insert into product (name, categoryId, description, price, status) values ('${product.name}', ${product.categoryId}, '${product.description}', ${product.price}, '${product.status}')`;
 	connection.query(query, (err, results) => {
 		if (!err) {
 			return res.status(200).json({ message: `Product added successfully` });
+		} else {
+			return res.status(500).json(err);
+		}
+	});
+});
+
+router.get('/get', auth.auth, (req, res, next) => {
+	let query = `select product.id, product.name, product.description, product.price, product.status, category.id as categoryId, category.name as categoryName from product inner join category on true where product.categoryId = category.id`;
+	connection.query(query, (err, results) => {
+		if (!err) {
+            return res.status(200).json(results.rows);
 		} else {
 			return res.status(500).json(err);
 		}
